d33f304e2df9bba2451ddc335678a4bbd03e9fcf
VfOrVeNdE <103868474+VfOrVeNdE@users.noreply.github.com>
2026-01-12 15:30:38 -0500

    update configuration
    add configurations

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,28 @@ export default defineConfig(({ mode }) => {
     define: {
       'import.meta.env.VITE_GEMINI_API_KEY': JSON.stringify(env.VITE_GEMINI_API_KEY),
     },
+
+    // configuration for manifest.json
+    build: {
+      // output directory 输出目录
+      outDir: 'dist',
+      // 防止文件带 hash (例如 main-x8s9.js)
+      rollupOptions: {
+        output: {
+          entryFileNames: 'main.js',
+          assetFileNames: (assetInfo) => {
+            
+            const infoName = assetInfo.names ? assetInfo.names[0] : assetInfo.name;
+            
+            if (infoName.names === 'style.css') {
+              return 'style.css';
+            } 
+            return '[name][extname]';
+            
+          },
+        },
+      },
+    },
   };
 
 });
